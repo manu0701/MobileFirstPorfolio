7eb8620e273cb024c2937bec07341b0ae2800f4c
manu0701 <44677330+manu0701@users.noreply.github.com>
2020-11-11 09:37:57 -0300

    Several changes

--- js/main.js
+++ js/main.js
@@ -1,39 +1,43 @@
-const nav = document.getElementById('nav-menu'),
+const navMenu = document.getElementById('nav-menu'),
     toggleMenu = document.getElementById('nav-toggle'),
     closeMenu = document.getElementById('nav-close')
 
 //Mostrar
 toggleMenu.addEventListener('click', () => {
-    nav.classList.toggle('show')
+    navMenu.classList.toggle('show')
 })
 
 //Ocultar
 closeMenu.addEventListener('click', () => {
-    nav.classList.remove('show')
+    navMenu.classList.remove('show')
 })
 
 //Remover menu
-const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll('.nav__link');
 
 function linkAction () {
-    nav.classList.remove('show')
+    navMenu.classList.remove('show')
 }
-
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
+//Scroll section active link
+
 const sections = document.querySelectorAll('section[id]')
+
 window.addEventListener('scroll', scrollActive)
+
 function scrollActive() {
-    const scrollY = window.pageYOffset
+    const scrollY = window.pageYOffset;
 
     sections.forEach(current => {
         const sectionHeight = current.offsetHeight;
         const sectionTop = current.offsetTop - 50;
-        sectionId = current.getAnimations('id')
+        sectionId = current.getAttribute('id')
+
         if(scrollY > sectionTop && scrollY <= sectionTop +sectionHeight) {
-            document.querySelector('.nav__menu a[href*='+ sectionId +']').classList.add('active')
+            document.querySelector('.nav__menu a[href*='+ sectionId +']').classList.add('active');
         } else {
-            document.querySelector('.nav__menu a[href*='+ sectionId +']').classList.remove('active')
+            document.querySelector('.nav__menu a[href*='+ sectionId +']').classList.remove('active');
         }
     })
 }
